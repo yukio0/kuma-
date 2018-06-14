9e79f008f869c705d9fec16122fe77dbd407c0a0
yukio0 <suugakun@gmail.com>
2018-06-15 03:31:58 +0900

    kuma-

---
+++ kuma-.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+while [ 1 ]
+clear
+do
+  echo ' 　　 ∩＿＿＿∩'
+  echo '　　  | ノ 　　 ヽ'
+  echo '　 　/ 　●　　　● |'
+  echo '　 |　　　( _●_)  ミ kuma-'
+  echo '　彡､　　　|∪|　　､＼'
+  echo '/　＿＿　 ヽノ　/´>  )'
+  echo '(＿＿＿）　　　/　(_／'
+  echo '　|　　　　　　 /'
+  echo '　|　　／＼　＼'
+  echo '　|　/　　　 )　 )'
+  echo '　∪　　　 （　 ＼'
+  echo '　　　　　　 ＼＿)'
+  sleep 1
+  clear
+  echo ' 　　 ∩＿＿＿∩'
+  echo '　　  | ノ 　　 ヽ'
+  echo '　 　/ 　●　　　● |'
+  echo '　 |　　　( _●_)  ミ       kuma-'
+  echo '　彡､　　　|∪|　  _､｀＼'
+  echo '/　＿＿　 ヽノ　/´  >  )'
+  echo '(＿＿＿）　　　/  　(_／'
+  echo '　|　　　　　　 /'
+  echo '　|　　／＼　＼'
+  echo '　|　/　　　 )　 )'
+  echo '　∪　　　 （　 ＼'
+  echo '　　　　　　 ＼＿)'
+  sleep 1
+done
